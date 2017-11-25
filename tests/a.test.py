40ae7522e3eb0b59a4bdf4a77e5cad03e9c3ab62
michaeldegroot <specamps@gmail.com>
2017-11-25 14:31:25 +0100

    travis: syntax errors should fail the test

--- tests/a.test.py
+++ tests/a.test.py
@@ -6,9 +6,10 @@ import cats
 
 class TestAddon(unittest.TestCase):
     def test_addon_enabled(self):
-        # test if addon got loaded correctly
-        # every addon must provide the "bl_info" dict
         self.assertIsNotNone(cats.bl_info)
+
+    def test_syntax_error(self):
+        self.assertRaises(SyntaxError, cats, "error_library")
 
 # we have to manually invoke the test runner here, as we cannot use the CLI
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestAddon)
