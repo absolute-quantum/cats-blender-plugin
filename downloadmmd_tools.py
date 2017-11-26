e4077c06c09fbe7ddc903c976a46ffe7634eb469
michaeldegroot <specamps@gmail.com>
2017-11-26 18:12:27 +0100

    travis: stringIO package in 2.7?

--- downloadmmd_tools.py
+++ downloadmmd_tools.py
@@ -4,6 +4,7 @@ import StringIO
 import os
 import wget
 
+# Download mmd_tools
 r = requests.get('https://github.com/powroupi/blender_mmd_tools/archive/dev_test.zip', stream=True)
 z = zipfile.ZipFile(StringIO.StringIO(r.content))
 z.extractall(os.path.join(os.path.dirname(__file__), 'tmp', 'mmd_tools'))
