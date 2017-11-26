4527021275be49a399839dbb4d3857b1cf773c06
michaeldegroot <specamps@gmail.com>
2017-11-26 18:08:59 +0100

    travis: blend files are now downloaded from dropbox, no repository dirtyness

--- downloadmmd_tools.py
+++ downloadmmd_tools.py
@@ -2,7 +2,11 @@ import requests
 import zipfile
 import StringIO
 import os
+import wget
 
 r = requests.get('https://github.com/powroupi/blender_mmd_tools/archive/dev_test.zip', stream=True)
 z = zipfile.ZipFile(StringIO.StringIO(r.content))
 z.extractall(os.path.join(os.path.dirname(__file__), 'tmp', 'mmd_tools'))
+
+# armature.mmd1.blend
+testfile = wget.download('https://www.dropbox.com/s/gvf33pxubugj3qe/armature.mmd1.blend?dl=1', os.path.join(os.path.dirname(__file__), 'tests', 'armatures', 'armature.mmd1.blend'))
