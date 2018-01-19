4bdd09a4b0380366ed6f185260222d87dc1991b7
michaeldegroot <specamps@gmail.com>
2018-01-19 21:20:05 +0100

    combine material bugfix

--- tools/material.py
+++ tools/material.py
@@ -154,7 +154,7 @@ class CombineMaterialsButton(bpy.types.Operator):
                 for tex_index, mtex_slot in enumerate(mat_slot.material.texture_slots):
                     if mtex_slot:
                         if mat_slot.material.use_textures[tex_index]:
-                            if hasattr(mtex_slot.texture, 'image') and bpy.data.materials[mat_slot.name].use_textures[tex_index]:
+                            if hasattr(mtex_slot.texture, 'image') and bpy.data.materials[mat_slot.name].use_textures[tex_index] and mtex_slot.texture.image:
                                 hash_this += mtex_slot.texture.image.filepath   # Filepaths makes the hash unique
                 hash_this += str(mat_slot.material.alpha)           # Alpha setting on material makes the hash unique
                 hash_this += str(mat_slot.material.specular_color)  # Specular color makes the hash unique
