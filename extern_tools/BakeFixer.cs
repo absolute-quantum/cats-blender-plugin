2f3af86b2e80b437d59df13a17b15ba1a794ddfe
Chelsea Jaggi <feilen1000@gmail.com>
2021-03-30 22:14:41 -0700

    Make BakeFixer.cs work correctly

--- extern_tools/BakeFixer.cs
+++ extern_tools/BakeFixer.cs
@@ -2,44 +2,51 @@ using System.Text;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
 using UnityEditor;
-using UnityEditor.PackageManager;
+#endif
 using System;
+using System.IO;
 
-[ExecuteInEditMode]
-public class BakeFixer : MonoBehaviour
+[InitializeOnLoad]
+public class BakeFixer : ScriptableObject
 {
-    [InitializeOnLoadMethod]
-    private static void InitializeOnLoad()
+#if UNITY_EDITOR
+    static BakeFixer()
     {
-    	Scene scene = SceneManager.GetActiveScene();
-    	var game_obj = scene.GetRootGameObjects();
-    	foreach (var obj in game_obj)
-    	{
-    		// For each armature on the scene who owns "Armature", "Static" and "Body",
-    		// get their SkinnedMeshRenderer components and edit the anchor point
-            // TODO: Also ensure they share the same folder as this script!
-    		if (obj.transform.Find("Armature") != null &&
-    		    obj.transform.Find("Body") != null &&
-    		    obj.transform.Find("Static") != null)
-    		{
-    			var body = obj.transform.Find("Body");
-    			var stat = obj.transform.Find("Static");
-    			var arm = obj.transform.Find("Armature");
-                if (arm.childCount > 0 && arm.Find("Hips") != null)
-                {
-    			    Debug.Log("Altering anchor point for: " + obj.name);
-                    var hips = arm.Find("Hips");
-    			    var smr_body = (SkinnedMeshRenderer)body.GetComponent(typeof(SkinnedMeshRenderer));
-    			    var smr_stat = (SkinnedMeshRenderer)body.GetComponent(typeof(SkinnedMeshRenderer));
-                
+        EditorApplication.hierarchyChanged += Update;
+        Debug.Log("BakeFixer loaded");
+    }
+
+    static void Update()
+    {
+        // Get the path of this script
+        Scene scene = SceneManager.GetActiveScene();
+        var game_obj = scene.GetRootGameObjects();
+        foreach (var obj in game_obj)
+        {
+            // For each armature on the scene who owns "Armature", "Static" and "Body",
+            // get their SkinnedMeshRenderer components and edit the anchor point
+            if (obj.transform.Find("Armature") != null &&
+                    obj.transform.Find("Body") != null &&
+                    obj.transform.Find("Static") != null)
+            {
+                var body = obj.transform.Find("Body");
+                var stat = obj.transform.Find("Static");
+                var arm = obj.transform.Find("Armature");
+                var smr_body = (SkinnedMeshRenderer)body.GetComponent(typeof(SkinnedMeshRenderer));
+                var smr_stat = (SkinnedMeshRenderer)body.GetComponent(typeof(SkinnedMeshRenderer));
+
+                if (arm.childCount > 0) {
+                    Debug.Log("Altering anchor point for: " + obj.name);
+                    var hips = arm.GetChild(0);
+
                     // Set lightprobe anchor
                     smr_body.probeAnchor = hips;
                     smr_stat.probeAnchor = hips;
-                } else {
-                    Debug.Log("Couldn't find Hips for: " + obj.name);
                 }
-	    	}	
-    	}
-   }
+            }	
+        }
+    }
+#endif
 }
