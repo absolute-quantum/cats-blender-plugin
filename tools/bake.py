b1784b94bfa1891201380b75132a03fd2622a129
Chelsea Jaggi <feilen1000@gmail.com>
2021-09-08 16:28:06 -0700

    Don't supersample world to tangent normals when we can use 'closest' instead

--- tools/bake.py
+++ tools/bake.py
@@ -431,7 +431,7 @@ class BakeButton(bpy.types.Operator):
         normal_apply_trans = context.scene.bake_normal_apply_trans
         diffuse_alpha_pack = context.scene.bake_diffuse_alpha_pack
         metallic_alpha_pack = context.scene.bake_metallic_alpha_pack
-        supersample_normals = context.scene.bake_pass_normal and context.scene.bake_use_decimation # Bake the intermediate step at 2x resolution. Probably best to leave this on.
+        supersample_normals = context.scene.bake_pass_normal and context.scene.bake_use_decimation and uv_overlap_correction == "UNMIRROR" # Bake the intermediate step at 2x resolution
         overlap_aware = False # Unreliable until UVP doesn't care about the island scale.
         emit_indirect = context.scene.bake_emit_indirect
         emit_exclude_eyes = context.scene.bake_emit_exclude_eyes
@@ -889,7 +889,7 @@ class BakeButton(bpy.types.Operator):
             # Bake normals in object coordinates
             if pass_normal:
                 for obj in collection.all_objects:
-                    if obj.type == 'MESH' and generate_uvmap:
+                    if obj.type == 'MESH' and generate_uvmap and supersample_normals:
                         obj.data.uv_layers.active = obj.data.uv_layers["CATS UV Super"]
                 bake_size = ((resolution * 2, resolution * 2) if
                              supersample_normals else
@@ -930,6 +930,9 @@ class BakeButton(bpy.types.Operator):
             normaltexnode = tree.nodes.new("ShaderNodeTexImage")
             if use_decimation:
                 normaltexnode.image = bpy.data.images["SCRIPT_world.png"]
+                # If not supersampling, sample SCRIPT_WORLD 1:1 so we don't blur it
+                if not supersample_normals:
+                    normaltexnode.interpolation = "Closest"
             normaltexnode.location.x -= 500
             normaltexnode.location.y -= 200
 
@@ -1002,6 +1005,7 @@ class BakeButton(bpy.types.Operator):
         if pass_normal:
             normaltexnode.image = bpy.data.images["SCRIPT_normal.png"]
             normalmapnode.space = "TANGENT"
+            normaltexnode.interpolation = "Linear"
         if pass_diffuse:
             diffusetexnode = tree.nodes.new("ShaderNodeTexImage")
             diffusetexnode.image = bpy.data.images["SCRIPT_diffuse.png"]
