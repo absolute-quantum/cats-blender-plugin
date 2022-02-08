81555773eb1311039933c2e9170f5600d40051e4
Chelsea Jaggi <feilen1000@gmail.com>
2022-02-07 21:38:54 -0800

    Improve vertex selection for mask application

--- tools/bake.py
+++ tools/bake.py
@@ -918,7 +918,7 @@ class BakeButton(bpy.types.Operator):
                     for group in ['LeftEye', 'lefteye', 'Lefteye', 'Eye.L', 'RightEye', 'righteye', 'Righteye', 'Eye.R']:
                         if group in obj.vertex_groups:
                             vgroup_idx = obj.vertex_groups[group].index
-                            if any(any(v_group.group == vgroup_idx for v_group in vert.groups) for vert in obj.data.vertices):
+                            if any(any(v_group.group == vgroup_idx and v_group.weight > 0.0 for v_group in vert.groups) for vert in obj.data.vertices):
                                 print("{} found in {}".format(group, obj.name))
                                 bpy.ops.object.mode_set(mode='EDIT')
                                 bpy.ops.uv.select_all(action='DESELECT')
@@ -1296,7 +1296,7 @@ class BakeButton(bpy.types.Operator):
                     Common.switch("OBJECT")
                     vgroup_idx = obj.vertex_groups[mod.vertex_group].index
                     for vert in obj.data.vertices:
-                        vert.select = any(group.group == vgroup_idx for group in vert.groups)
+                        vert.select = any(group.group == vgroup_idx and group.weight > 0.0 for group in vert.groups)
                     Common.switch("EDIT")
                     bpy.ops.mesh.delete(type="VERT")
 
