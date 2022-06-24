c8af9e0749fc199b0b1081eca298ab60365e0635
unknown <onanchew@gmail.com>
2022-06-23 20:29:29 -0400

    UI Update Part 2
    Hopefully this works in production, I fixed drop down menus being too big by replacing them with buttons that have search menus.

--- ui/custom.py
+++ ui/custom.py
@@ -11,6 +11,87 @@ from ..tools.translations import t
 
 
 @register_wrap
+class SearchMenuOperator_merge_armature_into(bpy.types.Operator):
+    bl_description = t('Scene.merge_armature_into.desc')
+    bl_idname = "scene.search_menu_merge_armature_into"
+    bl_label = ""
+    bl_property = "my_enum"
+
+    my_enum: bpy.props.EnumProperty(name=t('Scene.merge_armature_into.label'), description=t('Scene.merge_armature_into.desc'), items=Common.get_armature_list)
+    
+    def execute(self, context):
+        context.scene.merge_armature_into = self.my_enum
+        print(context.scene.root_bone)
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.invoke_search_popup(self)
+        return {'FINISHED'}
+
+@register_wrap
+class SearchMenuOperator_merge_armature(bpy.types.Operator):
+    bl_description = t('Scene.merge_armature.desc')
+    bl_idname = "scene.search_menu_merge_armature"
+    bl_label = t('Scene.root_bone.label')
+    bl_property = "my_enum"
+
+    my_enum: bpy.props.EnumProperty(name=t('Scene.merge_armature.label'), description= t('Scene.merge_armature.desc'), items=Common.get_armature_merge_list)
+    
+    def execute(self, context):
+        context.scene.merge_armature = self.my_enum
+        print(context.scene.root_bone)
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.invoke_search_popup(self)
+        return {'FINISHED'}
+        
+@register_wrap
+class SearchMenuOperator_attach_to_bone(bpy.types.Operator):
+    bl_description = t('Scene.attach_to_bone.desc')
+    bl_idname = "scene.search_menu_attach_to_bone"
+    bl_label = ""
+    bl_property = "my_enum"
+
+    my_enum: bpy.props.EnumProperty(name=t('Scene.attach_to_bone.label'), description= t('Scene.attach_to_bone.desc'), items=Common.get_bones_merge)
+    
+    def execute(self, context):
+        context.scene.attach_to_bone = self.my_enum
+        print(context.scene.root_bone)
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.invoke_search_popup(self)
+        return {'FINISHED'}
+        
+@register_wrap
+class SearchMenuOperator_attach_mesh(bpy.types.Operator):
+    bl_description = t('Scene.attach_mesh.desc')
+    bl_idname = "scene.search_menu_attach_mesh"
+    bl_label = ""
+    bl_property = "my_enum"
+
+    my_enum: bpy.props.EnumProperty(name=t('Scene.attach_mesh.label'), description= t('Scene.attach_mesh.desc'), items=Common.get_top_meshes)
+    
+    def execute(self, context):
+        context.scene.attach_mesh = self.my_enum
+        print(context.scene.root_bone)
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.invoke_search_popup(self)
+        return {'FINISHED'}
+
+
+
+
+
+
+@register_wrap
 class CustomPanel(ToolPanel, bpy.types.Panel):
     bl_idname = 'VIEW3D_PT_custom_v3'
     bl_label = t('CustomPanel.label')
@@ -64,11 +145,13 @@ class CustomPanel(ToolPanel, bpy.types.Panel):
 
             row = col.row(align=True)
             row.scale_y = 1.05
-            row.prop(context.scene, 'merge_armature_into', text=t('CustomPanel.mergeInto'), icon=globs.ICON_MOD_ARMATURE)
+            row.label(text=t('CustomPanel.mergeInto'))
+            row.operator(SearchMenuOperator_merge_armature_into.bl_idname,text = context.scene.merge_armature_into, icon=globs.ICON_MOD_ARMATURE)
             row = col.row(align=True)
             row.scale_y = 1.05
-            row.prop(context.scene, 'merge_armature', text=t('CustomPanel.toMerge'), icon_value=Supporter.preview_collections["custom_icons"]["UP_ARROW"].icon_id)
-
+            row.label(text=t('CustomPanel.toMerge'))
+            row.operator(SearchMenuOperator_merge_armature.bl_idname,text = context.scene.merge_armature, icon_value=Supporter.preview_collections["custom_icons"]["UP_ARROW"].icon_id)
+            
             if not context.scene.merge_same_bones:
                 found = False
                 base_armature = Common.get_armature(armature_name=context.scene.merge_armature_into)
@@ -81,7 +164,8 @@ class CustomPanel(ToolPanel, bpy.types.Panel):
                 if not found:
                     row = col.row(align=True)
                     row.scale_y = 1.05
-                    row.prop(context.scene, 'attach_to_bone', text=t('CustomPanel.attachToBone'), icon='BONE_DATA')
+                    row.label(text=t('CustomPanel.attachToBone'))
+                    row.operator(SearchMenuOperator_attach_to_bone.bl_idname, text=context.scene.attach_to_bone, icon='BONE_DATA') 
                 else:
                     row = col.row(align=True)
                     row.scale_y = 1.05
@@ -112,14 +196,17 @@ class CustomPanel(ToolPanel, bpy.types.Panel):
 
             row = col.row(align=True)
             row.scale_y = 1.05
-            row.prop(context.scene, 'merge_armature_into', text=t('CustomPanel.mergeInto'), icon=globs.ICON_MOD_ARMATURE)
+            row.label(text=t('CustomPanel.mergeInto'))
+            row.operator(SearchMenuOperator_merge_armature_into.bl_idname,text = context.scene.merge_armature_into, icon=globs.ICON_MOD_ARMATURE)
             row = col.row(align=True)
             row.scale_y = 1.05
-            row.prop(context.scene, 'attach_mesh', text=t('CustomPanel.attachMesh2'), icon_value=Supporter.preview_collections["custom_icons"]["UP_ARROW"].icon_id)
+            row.label(text=t('CustomPanel.attachMesh2'))
+            row.operator(SearchMenuOperator_attach_mesh.bl_idname, text=context.scene.attach_mesh, icon_value=Supporter.preview_collections["custom_icons"]["UP_ARROW"].icon_id) 
 
             row = col.row(align=True)
             row.scale_y = 1.05
-            row.prop(context.scene, 'attach_to_bone', text=t('CustomPanel.attachToBone'), icon='BONE_DATA')
+            row.label(text=t('CustomPanel.attachToBone'))
+            row.operator(SearchMenuOperator_attach_to_bone.bl_idname, text=context.scene.attach_to_bone, icon='BONE_DATA') 
 
             row = col.row(align=True)
             row.scale_y = 1.2
